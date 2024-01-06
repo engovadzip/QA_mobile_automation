a1b4d9cc3e8a70fad85534bea0aa0fdfab3aece7
Timir Gazizov <abzxcvvcxzba@yandex.ru>
2024-01-07 02:34:30 +0300

    Update code

--- tests/test_wikipedia_android_app.py
+++ tests/test_wikipedia_android_app.py
@@ -4,17 +4,13 @@ from selene import browser, have
 import allure
 
 
-def test_search_string_click(android_set):
+def test_search_article(android_set):
     with allure.step('Click search string'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
 
-
-def test_type_search_request(android_set):
     with allure.step('Type "John Cena"'):
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('John Cena')
 
-
-def test_open_article(android_set):
     with allure.step('Verify content found'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
